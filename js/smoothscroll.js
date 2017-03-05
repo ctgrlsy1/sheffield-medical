a4a742e9067aca9d226d230a5ef49f8c8ceceba0
Ktaylor1337 <katetaylor5155@gmail.com>
2017-03-05 11:46:48 +0000

    second-site

---
+++ js/smoothscroll.js
@@ -0,0 +1,72 @@
+$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$(document).ready(function() {
+
+  $('.link').on('click', function(e){
+    
+    var hash = $(this).attr('href');
+     $('html,body').animate({
+       scrollTop: $(hash).offset().top
+     }, 800);
+    
+    return false;
+  });
+});
